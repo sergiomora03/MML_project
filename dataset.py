a3c1d089b5d0c683530dd452d7ee47e8cef9f239
danielaruizl1 <da.ruizl1@uniandes.edu.co>
2023-07-26 17:15:01 -0500

    main

---
+++ dataset.py
@@ -0,0 +1,108 @@
+# Import libraries
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from skimage.io import imread
+import seaborn as sns
+import pandas as pd
+import os
+
+# Categories of the diferent lesions
+labels_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+# Get data
+csv_path = "data/HAM10000_metadata.csv"
+df_data=pd.read_csv(csv_path).set_index('image_id')
+df_data.dx=df_data.dx.astype('category',copy=True)
+df_data['label']=df_data.dx.cat.codes # Create a new column with the encoded categories
+df_data['lesion_type']= df_data.dx.map(labels_dict) # Create a new column with the lesion type
+df_data['path'] = "data/HAM10000_images/"+df_data.index + '.jpg' # Create a new column with the path to the image
+
+# Save relation between label and lesion_type
+label_list = df_data['label'].value_counts().keys().tolist()
+lesion_list = df_data['lesion_type'].value_counts().keys().tolist()
+label_to_lesion = dict(zip(label_list, lesion_list))
+
+# Visualize some examples
+
+lesion_groups = df_data.sort_values(["lesion_type"]).groupby("lesion_type")
+num_columns = 3
+num_rows = len(lesion_groups)
+fig, axs = plt.subplots(num_rows, num_columns, figsize=(12, 3*num_rows))  # Adjust figsize based on the number of rows
+
+for row, (lesion_name,images_id) in enumerate(lesion_groups):
+    for col, (_, c_row) in enumerate(images_id.sample(num_columns).iterrows()):
+        if col == 0:
+            axs[row,col].set_ylabel(lesion_name, fontsize=20)
+        ax = axs[row, col]
+        ax.imshow(imread(os.path.join("data", "HAM10000_images", c_row.name + '.jpg')))
+        ax.set_xticks([])  
+        ax.set_yticks([])
+
+plt.tight_layout()
+fig.savefig('lesion_examples.png', dpi=300)
+
+# Plot some histograms to caracterize the data
+plt.style.use('seaborn')
+
+plt.figure()
+lesion = df_data['lesion_type'].value_counts()
+plt.bar(lesion.keys().tolist(), lesion.tolist(), color=sns.color_palette("husl", 9))
+plt.title('Lesion type')
+plt.xticks(rotation=20)
+plt.savefig('lesion_histogram.png', dpi=300)
+
+plt.figure()
+localization = df_data['localization'].value_counts()
+plt.bar([x[:8] for x in localization.keys().tolist()],localization.tolist(), color=sns.color_palette("husl", 9))
+plt.title('Lesion localization')
+plt.xticks(rotation=30)
+plt.savefig('localization_histogram.png', dpi=300)
+
+plt.figure()
+age = df_data['age'].value_counts()
+plt.bar([int(x) for x in age.keys().tolist()], age.tolist(), color=sns.color_palette("husl", 9))
+plt.title('Patient Age')
+plt.xticks(rotation=30)
+plt.savefig('age_histogram.png', dpi=300)
+
+plt.figure()
+sex = df_data['sex'].value_counts()
+plt.bar(sex.keys().tolist(), sex.tolist(), color=sns.color_palette("husl", 9))
+plt.title("Patient Sex")
+plt.xticks(rotation=30)
+plt.savefig('sex_histogram.png', dpi=300)
+
+class HAM10000_Dataset(Dataset):
+
+    def __init__(
+        self,
+        df: pd.DataFrame):
+        self.df = df
+        self.toTensor = transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        # read dataframe row
+        row = self.df.iloc[idx]
+        # get image label and path
+        lesion_class = row["label"]
+        image_file = row['path']
+        if not os.path.exists(image_file):
+            raise FileNotFoundError(f"Couldn't find image {image_file}")
+        # read image 
+        image = imread(image_file)
+        image = self.toTensor(image)
+
+        return {'data': image, 'target': lesion_class}
+
---
+++ main.py
@@ -0,0 +1,255 @@
+#Import libraries
+from cgan_pytorch import Generator, Discriminator
+from sklearn import model_selection
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from dataset import HAM10000_Dataset
+from torch.autograd import Variable
+import pandas as pd
+import numpy as np
+import argparse
+import torch
+import os
+
+def get_main_parser():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+    parser.add_argument("--n_classes", type=int, default=7, help="number of classes for dataset")
+    parser.add_argument("--img_height", type=int, default=450, help="size of height image dimension")
+    parser.add_argument("--img_widht", type=int, default=600, help="size of widht image dimension")
+    parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+    opt = parser.parse_args()
+
+    return opt
+
+opt = get_main_parser()
+os.makedirs("images", exist_ok=True)
+
+# Categories of the diferent lesions
+labels_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+# Get data
+csv_path = "data/HAM10000_metadata.csv"
+df_data=pd.read_csv(csv_path).set_index('image_id')
+df_data.dx=df_data.dx.astype('category',copy=True)
+df_data['label']=df_data.dx.cat.codes # Create a new column with the encoded categories
+df_data['lesion_type']= df_data.dx.map(labels_dict) # Create a new column with the lesion type
+df_data['path'] = "data/HAM10000_images/"+df_data.index + '.jpg' # Create a new column with the path to the image
+
+# Save relation between label and lesion_type
+label_list = df_data['label'].value_counts().keys().tolist()
+lesion_list = df_data['lesion_type'].value_counts().keys().tolist()
+label_to_lesion = dict(zip(label_list, lesion_list))
+
+# Split data into train, validation and test
+test_size = 0.2
+val_size = 0.15
+df_train, df_test = model_selection.train_test_split(df_data, test_size=test_size)
+df_train_idx = df_train.reset_index().copy()
+df_train, df_val = model_selection.train_test_split(df_train_idx, test_size=val_size)
+
+# Configure data loader
+training_data = HAM10000_Dataset(df_train)
+validation_data = HAM10000_Dataset(df_val)
+test_data = HAM10000_Dataset(df_test)
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+val_dataloader = DataLoader(validation_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Loss functions
+adversarial_loss = torch.nn.MSELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+# Verify cuda
+cuda = True if torch.cuda.is_available() else False
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
+
+def sample_image(n_row, batches_done):
+    """Saves a grid of generated images ranging from 0 to n_classes"""
+    # Sample noise
+    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    # Get labels ranging from 0 to n_classes for n rows
+    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
+    labels = Variable(LongTensor(labels))
+    gen_imgs = generator(z, labels)
+    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+
+def val():
+    generator.eval()
+    discriminator.eval()
+    total_val_loss = 0.0
+    total_val_samples = 0
+
+    with torch.no_grad():
+        for i, (imgs, labels) in enumerate(val_dataloader):
+            batch_size = imgs.shape[0]
+
+            # Adversarial ground truths
+            valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+
+            # Configure input
+            real_imgs = Variable(imgs.type(FloatTensor))
+            labels = Variable(labels.type(LongTensor))
+
+            # Sample noise and labels as generator input
+            z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+            gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
+
+            # Generate a batch of images
+            gen_imgs = generator(z, gen_labels)
+
+            # Loss for real images
+            validity_real = discriminator(real_imgs, labels)
+            d_real_loss = adversarial_loss(validity_real, valid)
+
+            # Loss for fake images
+            validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+            d_fake_loss = adversarial_loss(validity_fake, valid)
+
+            # Total discriminator loss
+            d_loss = (d_real_loss + d_fake_loss) / 2
+
+            total_val_loss += d_loss.item() * batch_size
+            total_val_samples += batch_size
+
+    generator.train()
+    discriminator.train()
+    return total_val_loss / total_val_samples
+
+
+def test():
+    generator.eval()
+    discriminator.eval()
+    total_test_loss = 0.0
+    total_test_samples = 0
+
+    with torch.no_grad():
+        for i, (imgs, labels) in enumerate(test_dataloader):
+            batch_size = imgs.shape[0]
+
+            # Adversarial ground truths
+            valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+
+            # Configure input
+            real_imgs = Variable(imgs.type(FloatTensor))
+            labels = Variable(labels.type(LongTensor))
+
+            # Sample noise and labels as generator input
+            z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+            gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
+
+            # Generate a batch of images
+            gen_imgs = generator(z, gen_labels)
+
+            # Loss for real images
+            validity_real = discriminator(real_imgs, labels)
+            d_real_loss = adversarial_loss(validity_real, valid)
+
+            # Loss for fake images
+            validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+            d_fake_loss = adversarial_loss(validity_fake, valid)
+
+            # Total discriminator loss
+            d_loss = (d_real_loss + d_fake_loss) / 2
+
+            total_test_loss += d_loss.item() * batch_size
+            total_test_samples += batch_size
+
+    return total_test_loss / total_test_samples
+
+#  Training
+for epoch in range(opt.n_epochs):
+    for i, (imgs, labels) in enumerate(train_dataloader):
+
+        batch_size = imgs.shape[0]
+
+        # Adversarial ground truths
+        valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+        fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(imgs.type(FloatTensor))
+        labels = Variable(labels.type(LongTensor))
+
+        #  Train Generator
+        optimizer_G.zero_grad()
+
+        # Sample noise and labels as generator input
+        z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+        gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
+
+        # Generate a batch of images
+        gen_imgs = generator(z, gen_labels)
+
+        # Loss measures generator's ability to fool the discriminator
+        validity = discriminator(gen_imgs, gen_labels)
+        g_loss = adversarial_loss(validity, valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        #  Train Discriminator
+        optimizer_D.zero_grad()
+
+        # Loss for real images
+        validity_real = discriminator(real_imgs, labels)
+        d_real_loss = adversarial_loss(validity_real, valid)
+
+        # Loss for fake images
+        validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+        d_fake_loss = adversarial_loss(validity_fake, fake)
+
+        # Total discriminator loss
+        d_loss = (d_real_loss + d_fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.n_epochs, i, len(train_dataloader), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(train_dataloader) + i
+        if batches_done % opt.sample_interval == 0:
+            sample_image(n_row=10, batches_done=batches_done)
+
+        # Validation at the end of each epoch
+        val_loss = val()
+        print(f"Validation Loss after Epoch {epoch}: {val_loss}")
+
+# Testing after the training is complete
+test_loss = test()
+print(f"Test Loss: {test_loss}")
+
